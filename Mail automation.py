c65a544f5856ac71709bc4dd7ba1e049b6328f01
Christina Kötter <71084045+cnkoetter@users.noreply.github.com>
2021-04-14 14:17:06 +0200

    Update Mail automation.py

--- Mail automation.py
+++ Mail automation.py
@@ -1,16 +1,16 @@
 import os
-os.chdir("C:/Users/Michelle/Desktop/Arbeit/RWI/Mail")
+os.chdir("C:/Users/.../Mail")
 import csv
 from time import sleep
 import win32com.client as client
 import pathlib
 
-ethik_path = pathlib.Path('Ethikvotum.pdf')
-ethik_absolute = str(ethik_path.absolute())
+ethic_path = pathlib.Path('Ethicvotum.pdf')
+ethic_absolute = str(ethic_path.absolute())
 
 
 # open distribution list
-with open('peoplesc.csv', 'r', newline='') as f:
+with open('contact.csv', 'r', newline='') as f:
     reader = csv.reader(f)
     distro = [row for row in reader]
 
@@ -26,8 +26,8 @@ for chunk in chunks:
     for link, address in chunk:
         message = outlook.CreateItem(0)
         message.To = address
-        message.Subject = "Studie zu häuslicher Gewalt – Online-Umfrage und Daten zu Hilfegesuchen"
-        message.Attachments.Add(ethik_absolute)
+        message.Subject = "Research COVID-19"
+        message.Attachments.Add(ethic_absolute)
         html_body = """
 <pre><span class="pl-c">This is a very simple example, which includes an image from wikipedia.<br />Do not forget to include your links, names or any other information you would want to include with '<span style="color: #339966;"><strong>{}</strong></span>'.<br />Keep in mind that the placeholder for the link is now once used. So, you would need to use it once again or adjust <br />the <strong>'html_body.format(link,link)</strong>' to <strong>'html_body.format(link)'</strong>.<br /><br />Good luck and have fun!<br /><br /><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Fender_Jazz-Bass_1966.jpg/320px-Fender_Jazz-Bass_1966.jpg" alt="Jazz Bass" width="320" height="940" /><br /></span></pre>    """
         message.HTMLBody = html_body
